95a9949543b369d9eb33916f1f85cf9c4623f0ab
Eric Lu <penlume@gmail.com>
2015-11-08 18:37:25 -0500

    postView: setup for post rendering

---
+++ js/views/post-view.js
@@ -0,0 +1,39 @@
+(function() {
+
+var util = mcc.util;
+
+
+var PostView = mcc.PostView = mcc.View.extend({
+
+  className: 'post-view',
+
+  events: {
+    'click #image':               'triggerShowContent',
+  },
+
+  template: '\
+    <p id="injected">Start of post.</p>\
+    <div id="contentContainer"></div>\
+    <div id="commentListContainer"></div>\
+    <p id="injected">End of post.</p>
+  ',
+
+  initialize: function() {
+  },
+
+  render: function() {
+    this.$el.empty();
+
+    this.$el.append(_.template(this.template));
+
+    // TODO render content
+    content.render();
+    // TODO figure out how to append rendered version of things
+    this.$("#contentContainer").append(content.$el);
+
+    // TODO render comment list
+  }
+
+});
+
+})();
