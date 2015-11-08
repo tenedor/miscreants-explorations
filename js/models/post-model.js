a0ae36b6b40e88c00f02b8b1e258ce8f8bba675d
Eric Lu <penlume@gmail.com>
2015-11-08 16:25:53 -0500

    postModel: added getsetter flags

--- js/models/post-model.js
+++ js/models/post-model.js
@@ -12,11 +12,14 @@ var PostModel = mcc.PostModel = mcc.Model.extend({
   defaults: _.extend({}, mcc.Model.prototype.defaults, {
     content: undefined, // add getsetter
     comments: [] // add getsetter
-  })
+  }),
 
   addComment: function(content) {
     this.comments.append(content);
-  }
+  },
+
+  content: getSetterFlag,
+  comments: getSetterFlag
 
 });
 
