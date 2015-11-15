686b2e09b1841b7641656fbdbebc41e84f2bedc7
Maxwell Nye <maxnye@comcast.net>
2015-11-15 17:25:04 -0500

    magicResultsView: added framework in magic-results-veiw.js

---
+++ js/views/magic-results-view.js
@@ -0,0 +1,17 @@
+(function ( ) {
+
+var util = mcc.util;
+var View = mcc.View;
+
+var MagicResultsView = mcc.MagicResultsView = ContentView.extend({
+
+	className: 'magic-results-view',
+
+	template: _.template( ), //some stupid html string
+
+	render: function() {
+
+	}
+
+
+})()
