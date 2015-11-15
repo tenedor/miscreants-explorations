bb1e9d6516f244d49f75ffc7a6fdb004b1feb6a7
Maxwell Nye <maxnye@comcast.net>
2015-11-15 18:08:16 -0500

    magicResultsView: broke up magic results view

--- js/views/magic-results-view.js
+++ js/views/magic-results-view.js
@@ -3,9 +3,46 @@
 var util = mcc.util;
 var View = mcc.View;
 
-var MagicResultsView = mcc.MagicResultsView = ContentView.extend({
+var viewForResult = mcc.viewForResult = function(result) {
+	// pick the appropriate view here
+	//
+	// then instantiate and return it
+	if () {
+		return new PostListResultView({
+			model: result,
+			otherOptions: option,
+			otherOptions2: option2
+		});
+	} else {
+		return new CommentListResultView({
+			model: result
+			otherOptions: option
+			otherOptions2: option2
+	}	
 
-	className: 'magic-results-view',
+
+}
+
+var PostListResultView = mcc.PostListResultView = ContentView.extend({
+
+	className: 'post-list-results-view',
+
+	template: _.template( ), //some stupid html string
+
+	initialize: function() {
+		this.thing = thingFromArguments
+	}
+
+	render: function() {
+
+	}
+
+
+})
+
+var CommentListResultView = mcc.CommentListResultView = ContentView.extend({
+
+	className: 'comment-list-results-view',
 
 	template: _.template( ), //some stupid html string
 
@@ -13,5 +50,7 @@ var MagicResultsView = mcc.MagicResultsView = ContentView.extend({
 
 	}
 
+
+})
 
 })()
